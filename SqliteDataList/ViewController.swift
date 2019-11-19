fa04ffd6980d35428583b0bde6d279957f06b2bb
“Masam <masam.mahmood@tekhnelogos.com>
2019-11-19 13:43:37 +0300

    Initial Commit

---
+++ SqliteDataList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SqliteDataList
+//
+//  Created by Emre Türker on 19.11.2019.
+//  Copyright © 2019 MasamMahmood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
